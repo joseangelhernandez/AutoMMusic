75768273338991c61efd005db7519172e86a6750
Jose Angel Hernández <ja.hernandezds@gmail.com>
2024-03-08 18:34:36 -0400

    Add project files.

---
+++ PruebaAutoMMusic/ObjectRepository/LoginObjects.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaAutoMMusic.ObjectRepository
+{
+    public class LoginObjects
+    {
+        public static By input_Usuario = By.XPath("//*[@id='login-username']");
+        public static By input_Clave = By.XPath("//*[@id='login-password']");
+        public static By button_Entrar = By.XPath("//*[@id='main']/div/div[2]/div[3]/header/div[4]/div[1]/button[2]");
+        public static By login = By.XPath("//*[@id='login-button']");
+        public static By Buscar = By.XPath("//*[@id='main']/div/div[2]/div[1]/nav/div[1]/ul/li[2]/a");
+        public static By BuscarInput = By.XPath("//*[@id='main']/div/div[2]/div[3]/header/div[3]/div/div/form/input");
+        public static By tabPlaylist = By.XPath("//*[@id='main']/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/main/div[1]/div/div/div/div/div/a[2]");
+        public static By irPlaylist = By.XPath("//*[@id='searchPage']/div/div/div/div[1]/div[1]");
+        public static By reproducir = By.XPath("//*[@id='main']/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/main/div[1]/section/div[2]/div[2]/div[4]/div/div/div/div/div[1]/button");
+        public static By reproducirAleatorio = By.XPath("//*[@id='main']/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/main/div[1]/div/div/div/div/div/a[3]/button");
+        public static By reproducirRAND = By.XPath("//*[@id='main']/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/main/section/div[3]/div[4]/div/div/div/div/div[1]/button");
+        public static By reproducirmusicAleatorio = By.XPath("//*[@id='searchPage']/div/div/div/div[1]/div[1]/div[1]");
+        public static By UnaSola = By.XPath("//button[@class='Vz6yjzttS0YlLcwrkoUR tP0mccyU1WAa7I9PevC1'][@aria-checked='mixed']");
+        public static By Todas = By.XPath("//button[@class='Vz6yjzttS0YlLcwrkoUR tP0mccyU1WAa7I9PevC1']");
+        public static By busrarRandom = By.XPath("//button[@class='Vz6yjzttS0YlLcwrkoUR']");
+        public static By siguiente = By.XPath("//*[@id='main']/div/div[2]/div[2]/footer/div/div[2]/div/div[1]/div[2]/button[1]");
+    }
+}
---
+++ PruebaAutoMMusic/Program.cs
@@ -0,0 +1,139 @@
+﻿using PruebaAutoMMusic.ObjectRepository;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+string URL = "https://open.spotify.com/";
+string email = "kevinjrosario05@gmail.com";
+string pass = "Principito0102";
+
+// Obtiene la ruta del directorio del ejecutable
+string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+// Combina la ruta del directorio del ejecutable con el nombre del archivo
+string filePath = Path.Combine(baseDirectory, "config.txt");
+
+try
+{
+    // Lee todas las líneas del archivo
+    string[] lines = File.ReadAllLines(filePath);
+
+    // Asume que el archivo tiene al menos 3 líneas
+    email = lines[0];
+    pass = lines[1];
+
+    // Muestra los valores para verificar que se leyeron correctamente
+    Console.WriteLine($"Email: {email}");
+    Console.WriteLine($"Password: {pass}");
+}
+catch (IOException e)
+{
+    // Maneja el error si el archivo no se puede leer
+    Console.WriteLine($"No se pudo leer el archivo: {e.Message}");
+}
+
+
+Console.WriteLine("PRESIONE CUALQUIER TECLA PARA EMPEZAR...");
+Console.ReadKey();
+
+Console.WriteLine("AUTOMATIZACION EN CURSO");
+WebDriver driver = new ChromeDriver();
+
+Console.WriteLine("Logeando");
+driver.Navigate().GoToUrl(URL);
+
+IngresarUsuario(driver, email, pass);
+
+static void IngresarUsuario(WebDriver driver, string value, string pas)
+{
+        driver.FindElement(LoginObjects.button_Entrar).Click();
+        Thread.Sleep(3000);
+        driver.FindElement(LoginObjects.input_Usuario).SendKeys(value);
+        driver.FindElement(LoginObjects.input_Clave).SendKeys(pas);
+        driver.FindElement(LoginObjects.login).Click();
+
+        while (true)
+        {
+
+
+            if (true)
+            {
+                // Aquí colocas el código para ejecutar tu flujo en Selenium
+                // por ejemplo, llama a tu método que contiene el flujo
+                ReproducirPlaylist(driver);
+                Console.WriteLine("Flujo ejecutado con éxito.");
+
+                // Esperar 2 horas antes de ejecutar nuevamente
+                Thread.Sleep(20000);
+            }
+            else
+            {
+                //Console.WriteLine($"Error al ejecutar el flujo: {ex.Message}");
+                IngresarUsuario(driver, value, pas);
+                // En caso de error, puedes ajustar el tiempo de espera o manejar la situación según tus necesidades
+                Thread.Sleep(10000); // Esperar 2 minutos antes de intentar nuevamente en caso de error
+            }
+        }
+
+}
+
+static void ReproducirPlaylist(WebDriver driver)
+{
+    Thread.Sleep(4000);
+    driver.FindElement(LoginObjects.Buscar).Click();
+    Thread.Sleep(3000);
+    driver.FindElement(LoginObjects.BuscarInput).SendKeys("VmSongsFanSad");
+    Thread.Sleep(3000);
+    driver.FindElement(LoginObjects.tabPlaylist).Click();
+    Thread.Sleep(3000);
+    driver.FindElement(LoginObjects.irPlaylist).Click();
+    Thread.Sleep(3000);
+    driver.FindElement(LoginObjects.reproducir).Click();
+    Thread.Sleep(2000);
+    new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(d => d.FindElement(LoginObjects.siguiente).Displayed);
+    driver.FindElement(LoginObjects.siguiente).Click();
+
+    Thread.Sleep(10000);
+    driver.FindElement(LoginObjects.Buscar).Click();
+    Thread.Sleep(3000);
+    Random rnd = new Random();
+    List<string> correos = new List<string>() { "luiyitox me lo como", "luiyitox  no la presumas", "luiyitox   de que me vale negarlo" };
+    int correosIndex = rnd.Next(correos.Count);
+    string correosrandom = Convert.ToString(correos[correosIndex]);
+    driver.FindElement(LoginObjects.BuscarInput).SendKeys(correosrandom);
+    Thread.Sleep(3000);
+    driver.FindElement(LoginObjects.reproducirAleatorio).Click();
+    Thread.Sleep(3000);
+    driver.FindElement(LoginObjects.reproducirmusicAleatorio).Click();
+    Thread.Sleep(3000);
+    driver.FindElement(LoginObjects.reproducirRAND).Click();
+}
+static void correr(WebDriver driver, string value, string pas)
+{
+    while (true)
+    {
+
+
+        if (true)
+        {
+            // Aquí colocas el código para ejecutar tu flujo en Selenium
+            // por ejemplo, llama a tu método que contiene el flujo
+            IngresarUsuario(driver, value, pas);
+
+            Console.WriteLine("Flujo ejecutado con éxito.");
+
+            // Esperar 2 horas antes de ejecutar nuevamente
+            Thread.Sleep(10000);
+        }
+        else
+        {
+            //Console.WriteLine($"Error al ejecutar el flujo: {ex.Message}");
+            IngresarUsuario(driver, value, pas);
+            // En caso de error, puedes ajustar el tiempo de espera o manejar la situación según tus necesidades
+            Thread.Sleep(10000); // Esperar 2 minutos antes de intentar nuevamente en caso de error
+        }
+    }
+
+
+}
